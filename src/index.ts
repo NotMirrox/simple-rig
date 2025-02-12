3a752e5a5c94f18691361248cf7798d177057541
Mirrox <therealmirrox@gmail.com>
2025-02-12 23:47:19 +0300

    refactor:(r15 load rig) changed to r6

--- src/index.ts
+++ src/index.ts
@@ -1,217 +1,6 @@
-import { Lighting, Players, RunService } from "@rbxts/services"
+import { Players, RunService } from "@rbxts/services";
 
-type SimpleRigR15 = Model & {
-	LeftLowerArm: MeshPart & {
-		LeftElbowRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftElbow: Motor6D;
-		OriginalSize: Vector3Value;
-		LeftWristRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	LeftFoot: MeshPart & {
-		OriginalSize: Vector3Value;
-		LeftAnkleRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftAnkle: Motor6D;
-	};
-	RightHand: MeshPart & {
-		RightWristRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightWrist: Motor6D;
-		RightGripAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-	};
-	HumanoidRootPart: Part & {
-		RootRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-	};
-	RightLowerLeg: MeshPart & {
-		RightKneeRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightAnkleRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightKnee: Motor6D;
-		OriginalSize: Vector3Value;
-	};
-	LeftUpperLeg: MeshPart & {
-		OriginalSize: Vector3Value;
-		LeftHip: Motor6D;
-		LeftHipRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftKneeRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	LeftLowerLeg: MeshPart & {
-		OriginalSize: Vector3Value;
-		LeftKnee: Motor6D;
-		LeftAnkleRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftKneeRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	RightUpperArm: MeshPart & {
-		OriginalSize: Vector3Value;
-		RightElbowRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightShoulderRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightShoulderAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightShoulder: Motor6D;
-	};
-	Head: Part & {
-		HatAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-		NeckRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		FaceFrontAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		face: Decal;
-		HairAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		Neck: Motor6D;
-		Mesh: SpecialMesh & {
-			OriginalSize: Vector3Value;
-		};
-		FaceCenterAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	UpperTorso: MeshPart & {
-		RightCollarAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		BodyBackAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		NeckRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftCollarAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-		Waist: Motor6D;
-		RightShoulderRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		BodyFrontAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		WaistRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftShoulderRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		NeckAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	LeftUpperArm: MeshPart & {
-		LeftShoulderRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftShoulder: Motor6D;
-		LeftShoulderAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftElbowRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-	};
-	RightLowerArm: MeshPart & {
-		RightWristRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-		RightElbow: Motor6D;
-		RightElbowRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	LeftHand: MeshPart & {
-		LeftWrist: Motor6D;
-		LeftGripAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-		LeftWristRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	Humanoid: Humanoid & {
-		HumanoidDescription: HumanoidDescription;
-	};
-	LowerTorso: MeshPart & {
-		WaistCenterAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		LeftHipRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		Root: Motor6D;
-		RootRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightHipRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-		WaistRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		WaistBackAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		WaistFrontAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	RightUpperLeg: MeshPart & {
-		RightKneeRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		OriginalSize: Vector3Value;
-		RightHip: Motor6D;
-		RightHipRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-	};
-	RightFoot: MeshPart & {
-		RightAnkleRigAttachment: Attachment & {
-			OriginalPosition: Vector3Value;
-		};
-		RightAnkle: Motor6D;
-		OriginalSize: Vector3Value;
-	};
-};
-
-type SimpleRigR6 = Model & {
+export type SimpleRigR6 = Model & {
 	["Left Leg"]: MeshPart & {
 		["Left Leg"]: Weld;
 		LeftFootAttachment: Attachment;
@@ -306,11 +95,61 @@ type SimpleRigR6 = Model & {
 	Accessories: Folder;
 };
 
-const r6Rig = script.WaitForChild("r6Rig") as SimpleRigR6;
-const r15Rig = script.WaitForChild("r15Rig") as SimpleRigR15;
+type LoadRig = Model & {
+	["Left Leg"]: Part & {
+		LeftFootAttachment: Attachment;
+	};
+	Humanoid: Humanoid & {
+		HumanoidDescription: HumanoidDescription;
+	};
+	["Right Arm"]: Part & {
+		RightShoulderAttachment: Attachment;
+		RightGripAttachment: Attachment;
+	};
+	Head: Part & {
+		HatAttachment: Attachment;
+		FaceFrontAttachment: Attachment;
+		HairAttachment: Attachment;
+		face: Decal;
+		Mesh: SpecialMesh;
+		FaceCenterAttachment: Attachment;
+	};
+	["Right Leg"]: Part & {
+		RightFootAttachment: Attachment;
+	};
+	Torso: Part & {
+		RightCollarAttachment: Attachment;
+		WaistCenterAttachment: Attachment;
+		BodyBackAttachment: Attachment;
+		Neck: Motor6D;
+		LeftCollarAttachment: Attachment;
+		["Left Hip"]: Motor6D;
+		roblox: Decal;
+		["Right Hip"]: Motor6D;
+		["Left Shoulder"]: Motor6D;
+		["Right Shoulder"]: Motor6D;
+		BodyFrontAttachment: Attachment;
+		WaistBackAttachment: Attachment;
+		WaistFrontAttachment: Attachment;
+		NeckAttachment: Attachment;
+	};
+	HumanoidRootPart: Part & {
+		RootJoint: Motor6D;
+		RootAttachment: Attachment;
+	};
+	["Left Arm"]: Part & {
+		LeftGripAttachment: Attachment;
+		LeftShoulderAttachment: Attachment;
+	};
+};
 
-const loadRig = RunService.IsServer() ? r15Rig : r15Rig.Clone();
-loadRig.Parent = Lighting;
+const r6Rig = script.WaitForChild("rig-r6") as SimpleRigR6;
+let loadRig = script.WaitForChild("load-rig") as LoadRig;
+if (RunService.IsClient()) {
+	const clone = loadRig.Clone();
+	loadRig.Destroy();
+	loadRig = clone;
+}
 
 const BODY_PARTS = {
 	"Left Arm": "LeftArm",
@@ -432,4 +271,3 @@ export class SimpleRig {
 		this.model.Destroy();
 	}
 }
-
